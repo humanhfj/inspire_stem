216a7754d3324db872dc21ea12b68ed529a7093b
humanhfj <vincentmorarakaunda@gmail.com>
2024-02-22 10:13:36 +0300

    loops.py

---
+++ week_1/strings.py
@@ -0,0 +1,36 @@
+#string in python
+#Date
+#Name : Vini
+city = "nairobi"
+
+#Convert to upper case
+print(city)
+print("\n\n") #prints a new line
+print(city.upper())
+
+name = "VINCENT MORARA"
+print(name)
+print(name.lower())#converts string to lower case
+
+town ="    Naiva sha   "
+print(town)
+print("\t")#new tab
+print(town.strip()) 
+f_name ="John"
+s_name ="Wamai"
+full_name = f_name + s_name
+print(full_name)
+
+ #replacing a character 
+fruit ="Orange00000"
+print(fruit.replace("0","Y"))   
+
+subject ="Phydics:Sciences"
+print(subject.split(":"))
+
+age = 50
+height = 1.5
+print("I am {}years old and {} meters tall".format(age,height))
+
+
+
---
+++ week_2/loops.py
@@ -0,0 +1,14 @@
+# lops and literations
+#Date
+#Name
+
+
+# For loop 
+# Whlie loop
+
+for number in range(0,10):
+    print(number)
+    #squares of the number
+    for x in range(0,11):
+        print(str(x**2)  + "\t",end=" ")  #squares
+        print("\t")
