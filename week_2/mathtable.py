96e09d4877245f89d6c0f7c16e35c76c15e020a8
humanhfj <vincentmorarakaunda@gmail.com>
2024-02-23 08:08:35 +0300

    mathtable.py

---
+++ week_2/mathtable.py
@@ -0,0 +1,29 @@
+# this is a program to generate amathe matical table
+# date : 22/2/2024
+# name : vincent morara
+
+for i in range(1,11):
+
+    a=i*1
+    b=i*2
+    c=i*3
+    d=i*4
+    e=i*5
+    f=i*6
+    g=i*7
+    h=i*8
+    j=i*9
+    k=i*10
+
+    print(str(a) + "\t",end ="")
+    print(str(b) + "\t",end ="")
+    print(str(c) + "\t",end ="")
+    print(str(d) + "\t",end ="") 
+    print(str(e) + "\t",end ="")
+    print(str(f) + "\t",end ="") 
+    print(str(g) + "\t",end ="")
+    print(str(h) + "\t",end ="")
+    print(str(i) + "\t",end ="")
+    print(str(j) + "\t",end ="") 
+    print(str(k) + "\t",end ="")
+    print("\n")
