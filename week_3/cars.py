e26074ade4a63c9915ae5d680337ceb8cd6589f5
humanhfj <vincentmorarakaunda@gmail.com>
2024-02-29 08:39:27 +0300

    hobbies.py

---
+++ week_3/cars.py
@@ -0,0 +1,6 @@
+
+car = {"make" : "suzuki" , "type" :  "urus" , "colour" : "black" ,"year" : "2024","acceleration":"0-150 in 2 seconds","top speed":"500 kph"}
+for key,value in car.items():
+    print("\n")
+    print(key)
+    print(value)
---
+++ week_3/hobbies.py
@@ -0,0 +1,4 @@
+# tuple my hobbies
+hobbies=("football","eating","swimming","partying","watching movies","playing games")
+for hobby in hobbies:
+    print(hobby)
